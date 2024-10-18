db96848f991c056e4af8e3e327aa871980459fb5
Stephen DeLorme <stephen@d.elor.me>
2024-10-18 18:00:57 -0400

    added coordinates lookup

--- app/atlien/[coordinates]/page.tsx
+++ app/atlien/[coordinates]/page.tsx
@@ -1,18 +1,34 @@
 import { findAtlien } from "@/lib/atlien";
 
-export default function Atlien() {
+export default function Atlien({ params }: {params: { coordinates: string } }) {
 
-    const atlien = findAtlien(1,122.454545);
+  const coordinatesString = params.coordinates;
+
+  // Decode the URI component to get the original string
+  const decodedCoordinatesString = decodeURIComponent(coordinatesString);
+
+  // Remove the '@' and split the string by the comma
+  const [latitudeStr, longitudeStr] = decodedCoordinatesString.slice(1).split(',');
+
+  // Convert the strings to numbers
+  const latitude = parseFloat(latitudeStr);
+  const longitude = parseFloat(longitudeStr);
+
+  // Use the findAtlien function with the extracted coordinates
+  const atlien = findAtlien(latitude, longitude);
 
   return (
     <div className="max-w-4xl w-full mx-auto p-8 mt-8 border border-gray-500 rounded">
+
       <h1 className="text-4xl">{atlien.adjective} {atlien.name}</h1>
 
-        <h2>Unique Number</h2>
-        <p>{atlien.uniqueNumber}</p>
+      <h2>Coordinates</h2>
+      <p>@{latitude},{longitude}</p>
 
         <h2>From</h2>
         <p>{atlien.neighborhood}</p>
+
+
 
         <h2>Species</h2>
         <p>{atlien.species}</p>
