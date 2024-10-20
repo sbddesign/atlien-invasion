007748668d58071001605cabd206c80c9698d00b
Stephen DeLorme <stephen@d.elor.me>
2024-10-20 09:05:23 -0400

    removed old user create

--- components/NewUser.tsx
+++ components/NewUser.tsx
@@ -2,16 +2,10 @@
 
 "use client";
 
-import { prisma } from '@/lib/prisma';
-
 export default function UsersPage() {
-  const createUser = () => {
-    prisma.user.create({data: { email: 'stephen@d.elor.me' }});
-  }
-
   return (
     <div>
-      <button onClick={createUser}>New User</button>
-      </div>
+      New User Create might go here
+    </div>
     )
 }
