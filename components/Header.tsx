d5dd362bd4af914bb762dcfaa54bbfc21256333f
Stephen DeLorme <stephen@d.elor.me>
2024-10-20 07:55:56 -0400

    header improvements

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,11 @@
 "use client";
+import { useState } from 'react';
+import { MenuIcon } from '@bitcoin-design/bitcoin-icons-react/filled';
+import Link from 'next/link';
 
 export default function Header() {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
     const menu = [
         {
             href: '/',
@@ -22,16 +27,40 @@ export default function Header() {
             href: '/bounty/new',
             text: 'New Bounty'
         }
-    ]
+    ];
 
-    return(
-        <header>
-            <p>ATLien Invasion</p>
-            <nav>
-                <ul>
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
+
+    const closeNav = () => {
+        setIsNavVisible(false);
+    };
+
+    return (
+        <header className="items-center border-b border-gray-200 lg:flex flex-row justify-between">
+            <div className="max-md:w-full flex flex-row justify-between p-4 lg:flex-initial">
+                <Link href="/" className="font-bold m-0 no-underline text-black">ATLien Invasion</Link>
+                <button 
+                    title="Show Menu" 
+                    aria-hidden="true" 
+                    className="lg:hidden" 
+                    onClick={toggleNav}
+                >
+                    <MenuIcon className="w-6 h-6" />
+                </button>
+            </div>
+            <nav className={`lg:flex max-md:border-t lg:w-full lg:flex-1 max-md:border-gray-200 ${isNavVisible ? 'block' : 'hidden'}`}>
+                <ul className="flex flex-col lg:flex-row lg:items-center w-full justify-end">
                     {menu.map((item, index) => (
-                        <li key={index}>
-                            <a href={item.href}>{item.text}</a>
+                        <li key={index} className="lg:ml-4">
+                            <Link 
+                                href={item.href} 
+                                className="p-4 block no-underline font-medium text-lg hover:bg-gray-50"
+                                onClick={closeNav}
+                            >
+                                {item.text}
+                            </Link>
                         </li>
                     ))}
                 </ul>
