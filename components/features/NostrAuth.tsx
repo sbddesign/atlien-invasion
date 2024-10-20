f744682a02cf803b773918b74b2b0a9c172af20a
Stephen DeLorme <stephen@d.elor.me>
2024-10-20 08:50:06 -0400

    get npub

--- components/features/NostrAuth.tsx
+++ components/features/NostrAuth.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect } from 'react';
+import { nip19 } from 'nostr-tools';
 
 // Add this declaration at the top of your file or in a separate declaration file
 declare global {
@@ -15,6 +16,8 @@ export default function NostrAuth() {
         if (typeof window !== 'undefined' && window.nostr) {
             window.nostr.getPublicKey().then((publicKey) => {
                 console.log(publicKey);
+                let npub = nip19.npubEncode(publicKey);
+                console.log(npub);
             }).catch((error) => {
                 console.error('Error getting public key:', error);
             });
