c4f3a8f74ff08ec5471ab0072395bd2c64c486e2
Stephen DeLorme <stephen@d.elor.me>
2024-10-21 17:56:15 -0400

    list bounties

--- app/bounties/page.tsx
+++ app/bounties/page.tsx
@@ -1,7 +1,20 @@
-export default function Bounties() {
+import { prisma } from '@/lib/prisma';
+
+export default async function Bounties() {
+  const bounties = await prisma.bounty.findMany();
+
   return (
     <div className="">
       <h1>Bounties</h1>
+      <ul>
+        {bounties.map((bounty) => (
+          <li key={bounty.id}>
+            <p>Coordinates: {bounty.coordinates}</p>
+            <p>Amount: {bounty.amount}</p>
+            <p>Claimed: {bounty.claimedById ? 'Yes' : 'No'}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
